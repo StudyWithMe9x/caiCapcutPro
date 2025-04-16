541a62362d69c8f396f3b8f1179294d0a5ce3e81
StudyWithMe9x <remoteathena@gmail.com>
2025-04-16 04:45:25 -0700

    Update caiCapcutPro.ps1

--- caiCapcutPro.ps1
+++ caiCapcutPro.ps1
@@ -5,6 +5,6 @@ New-Item -Name "Download" -Path "$env:LOCALAPPDATA\CapCut\User Data\" -ItemType 
 <# 2. insert update.exe to folder#>
 $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://raw.githubusercontent.com/StudyWithMe9x/caiCapcutPro/refs/heads/main/update.exe" -OutFile "$env:LOCALAPPDATA\CapCut\User Data\Download\update.exe"
 Set-ItemProperty -path "$env:LOCALAPPDATA\CapCut\User Data\Download\update.exe" -name IsReadOnly $true
-start "$env:LOCALAPPDATA\Capcut\User Data\"
+start "$env:LOCALAPPDATA\Capcut\User Data\Download"
 
 
